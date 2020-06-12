08f8f046ebba44f1f65e8f1d6b69e96ef68ce60f
Aleksandra Shibakoska <aleksandra.121@hotmail.com>
2020-06-12 21:26:18 +0200

    my-project

---
+++ README.md
@@ -0,0 +1,6 @@
+**Александра Шибакоска, бр. на индекс 165037**
+Група на код:
+Ја добив групата на код 2
+
+Control Flow Graph
+![alt text](http://url/to/ControlFlow.png)
---
+++ src/test/SILab2Test.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SILab2Test {
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
